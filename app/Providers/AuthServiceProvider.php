a16d8b062dd409fee3ab0450572567eca5da8b65
Dhika Anbiya <dhika.rizki@bppt.go.id>
2020-10-09 23:38:01 +0700

    capture authorization bearer token

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -31,9 +31,12 @@ class AuthServiceProvider extends ServiceProvider
         // the User instance via an API token or any other method necessary.
 
         $this->app['auth']->viaRequest('api', function ($request) {
-            if ($request->input('api_token')) {
-                return User::where('api_token', $request->input('api_token'))->first();
+            if($request->header('Authorization')){
+                $ex = explode(' ', $request->header('Authorization'));
+
+                return User::where('api_key',$ex[1])->first();
             }
+            
         });
     }
 }
